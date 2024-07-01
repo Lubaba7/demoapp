b930bda0c614f6f115f827d5bdb20cd51d2d8e75
Saada0 <lubaba980@gmail.com>
2024-06-30 19:54:47 -0700

    insert employee data

--- Backend/app.js
+++ Backend/app.js
@@ -22,6 +22,34 @@ connection.connect((err)=> {
     console.log("MYSQL Connected...");
 })
 
+app.use(express.json())
+
+app.post("/add-employee", (req, res) => {
+    const {first_name, last_name, email, password} = req.body
+    console.log(first_name);
+    const sql = `INSERT INTO employ_test(first_name, last_name,	email, password) VALUES ("${first_name}", "${last_name}", "${email}", "${password}")` ;
+
+
+    connection.query(sql, (err, result)=>{
+        if(err) throw err;
+        console.log("data successfully iserted");
+    })
+    
+    
+    const response = {
+        status: "success",
+        message: "Employee add successfully"
+    }
+
+    res.status(201).json(response)
+})
+
+
+
+
+
+
+
 app.listen(port, (err, result) => {
     if(err){
         console.log(err);
